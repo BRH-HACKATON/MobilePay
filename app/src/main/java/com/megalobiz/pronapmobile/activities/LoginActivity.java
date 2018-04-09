1b2e3b44f4f0e04ccfeea34a54786a29563bf329
KeitelDOG <keiteldog@gmail.com>
2018-04-09 16:42:30 -0400

    Implement Flow between Login, Register and Main Activity

--- app/src/main/java/com/megalobiz/pronapmobile/activities/LoginActivity.java
+++ app/src/main/java/com/megalobiz/pronapmobile/activities/LoginActivity.java
@@ -1,13 +1,116 @@
 package com.megalobiz.pronapmobile.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.megalobiz.pronapmobile.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_REGISTER = 0;
+
+    private AppCompatButton buttonLogin;
+    private TextView tvRegister;
+
+    private TextInputEditText editTextEmail;
+    private TextInputEditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonLogin = findViewById(R.id.btnLogin);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        tvRegister = findViewById(R.id.tvRegister);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
     }
+
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        // TODO : Validate inputs
+        /*if (!validate()) {
+            onLoginFailed();
+            return;
+        }*/
+
+        buttonLogin.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        // TODO: Implement your own registration logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 2000);
+    }
+
+    public void onLoginSuccess() {
+        buttonLogin.setEnabled(true);
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        buttonLogin.setEnabled(true);
+    }
+
+    public void register() {
+        Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivityForResult(i, REQUEST_REGISTER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_REGISTER) {
+            if (resultCode == RESULT_OK) {
+                // By default we just finish the Activity and log them in automatically
+                onLoginSuccess();
+                this.finish();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/megalobiz/pronapmobile/activities/RegisterActivity.java
+++ app/src/main/java/com/megalobiz/pronapmobile/activities/RegisterActivity.java
@@ -1,13 +1,90 @@
 package com.megalobiz.pronapmobile.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.megalobiz.pronapmobile.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_REGISTER = 0;
+
+    private AppCompatButton buttonRegister;
+
+    private TextInputEditText editTextFullName;
+    private TextInputEditText editTextEmail;
+    private TextInputEditText editTextPassword;
+    private TextInputEditText editTextPasswordConf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        buttonRegister = findViewById(R.id.btnRegister);
+        editTextFullName = findViewById(R.id.fullName);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        editTextPasswordConf = findViewById(R.id.passwordConfirmation);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+    public void register() {
+        Log.d(TAG, "Register");
+
+        // TODO : Validate inputs
+        /*if (!validate()) {
+            onRegistrationFailed();
+            return;
+        }*/
+
+        buttonRegister.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this,
+                R.style.AppTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Registering...");
+        progressDialog.show();
+
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onRegisterSuccess();
+                        // onRegisterFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 2000);
+    }
+
+    public void onRegisterSuccess() {
+        buttonRegister.setEnabled(true);
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    public void onRegisterFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        buttonRegister.setEnabled(true);
     }
 }
